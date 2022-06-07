bc50a50c9c21fad7ab91fc7fb021d7fcceff3520
EgorKokhnenko <kax.egor@mail.ru>
2022-06-07 13:18:13 +0300

    Updates

---
+++ precache-manifest.e6f86c11e2fc427e05a6716f46a060f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f145f8698ee60a428192565bbc39b2b6",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "38e7807d0787accef01e",
+    "url": "/counter/static/css/main.a1323596.chunk.css"
+  },
+  {
+    "revision": "c31cc7dacd6214e54779",
+    "url": "/counter/static/js/2.d960f734.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/counter/static/js/2.d960f734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e7807d0787accef01e",
+    "url": "/counter/static/js/main.f5a1b563.chunk.js"
+  },
+  {
+    "revision": "1434de11b0502c2d193d",
+    "url": "/counter/static/js/runtime-main.4e2cabff.js"
+  }
+]);
